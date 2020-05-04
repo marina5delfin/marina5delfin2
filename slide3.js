051faa2013e190f0882b358d761168a9a4f5468f
marina5delfin <34003936+marina5delfin@users.noreply.github.com>
2020-05-04 23:26:19 +0300

    Add files via upload

---
+++ slide3.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{1D0A46C2-2CE2-4954-BD57-F5F982D428E1}'];loadHandler&&loadHandler(2, '<div id="spr0_1c0e4cf"><div id="spr1_1c0e4cf" class="kern slide"><img id="img0_1c0e4cf" src="data/img1.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_1c0e4cf" class="kern slide"><div id="vd0_1c0e4cf"></div></div></div>');})();
